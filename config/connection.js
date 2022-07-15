573fba2b70d3d04e138728b9118cab596cebedbe
san-geetha-s <101059528+san-geetha-s@users.noreply.github.com>
2022-07-15 08:17:20 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ const state={
 }
 
 module.exports.connect=function(done){
-//     const url=`mongodb+srv://sangeetha:<Q1hxvgIoeIKcuW7s>@cluster0.cxlnuak.mongodb.net/?retryWrites=true&w=majority`
+    const url=`mongodb+srv://sangeetha:<Q1hxvgIoeIKcuW7s>@cluster0.cxlnuak.mongodb.net/?retryWrites=true&w=majority`
 //     mongosh "mongodb+srv://cluster0.cxlnuak.mongodb.net/myFirstDatabase" --apiVersion 1 --username sangeetha
 //    const url=' mongodb+srv://<credentials>@cluster0.cxlnuak.mongodb.net/myFirstDatabase?appName=mongosh+1.5.0'
     
-    const url = 'mongodb://localhost:27017';
+//     const url = 'mongodb://localhost:27017';
     const dbname='shopping'
 
 
